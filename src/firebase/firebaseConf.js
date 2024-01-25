9bce0b09765d640f5f5a9aad2ae4ff1b1387a403
afaqkhan707 <bsse5482@gmail.com>
2024-01-25 11:59:43 +0500

    removed env

--- src/firebase/firebaseConf.js
+++ src/firebase/firebaseConf.js
@@ -16,15 +16,22 @@ import {
 } from '@env';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
+// const firebaseConfig = {
+//   apiKey: API_KEY,
+//   authDomain: AUTH_DOMAIN,
+//   projectId: PROJECT_ID,
+//   storageBucket: STORAGE_BUCKET,
+//   messagingSenderId: MESSAGING_SENDER_ID,
+//   appId: APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: 'AIzaSyDO9tuoshNM4m8HocmVPkVkeR5rS4JTOEo',
+  authDomain: 'todo-reactnativefirebase.firebaseapp.com',
+  projectId: 'todo-reactnativefirebase',
+  storageBucket: 'todo-reactnativefirebase.appspot.com',
+  messagingSenderId: '526201475157',
+  appId: '1:526201475157:web:f4664711cc641176fe44dc',
 };
-
 const app = initializeApp(firebaseConfig);
 
 const firestore = getFirestore(app);
@@ -38,8 +45,3 @@ const auth = initializeAuth(app, {
 });
 
 export { auth, firestore };
-
-
-
-
-

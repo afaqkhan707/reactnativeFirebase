1e8fb3cee9e969b257f28f7392bda47ab1f468dc
afaqkhan707 <bsse5482@gmail.com>
2024-01-25 12:23:30 +0500

    changes

--- src/firebase/firebaseConf.js
+++ src/firebase/firebaseConf.js
@@ -6,24 +6,10 @@ import {
   initializeAuth,
   getReactNativePersistence,
 } from 'firebase/auth';
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-} from '@env';
+
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// const firebaseConfig = {
-//   apiKey: API_KEY,
-//   authDomain: AUTH_DOMAIN,
-//   projectId: PROJECT_ID,
-//   storageBucket: STORAGE_BUCKET,
-//   messagingSenderId: MESSAGING_SENDER_ID,
-//   appId: APP_ID,
-// };
+
 const firebaseConfig = {
   apiKey: 'AIzaSyDO9tuoshNM4m8HocmVPkVkeR5rS4JTOEo',
   authDomain: 'todo-reactnativefirebase.firebaseapp.com',
